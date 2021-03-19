582c723ee812fc32e1da5bc489780e5bb418db54
sena.umemura <sna.umemura@gmail.com>
2021-03-19 16:25:27 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 梅村世奈 on 2021/03/19.
+//  Copyright © 2021 sena.umemura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
